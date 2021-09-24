cfc78c17e493033362ff16f342ef6017677aae88
RubySrivastava <ruby.srivas1984@gmail.com>
2021-09-24 08:48:26 +0000

    Upload images and create data file

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
 import Cart from './components/Cart'
 
 const App = () => {
